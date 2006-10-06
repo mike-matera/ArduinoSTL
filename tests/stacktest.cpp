c856e49ccd73330745f08625389e6f4ea1630a17
Garrett Kajmowicz <gkajmowi@tbaytel.net>
2006-10-06 21:24:04 +0000

    Begin conversion to a unittest - like framework.

--- tests/stacktest.cpp
+++ tests/stacktest.cpp
@@ -1,20 +1,71 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include "testframework.h"
 
-int main(){
 
-
-	std::cout<<"Starting stack test" << std::endl;
+unsigned long int checkStackSize(){
 	std::stack<int, std::deque<int> > s;
 	s.push(42);
 	s.push(101);
 	s.push(69);
-	std::cout << "Size of stack: " << s.size() << std::endl;
-	while(!s.empty()){
-		std::cout << s.top() << " " << s.size() << std::endl;
-		s.pop();
+	return s.size();
+}
+
+bool checkStackPop(){
+	std::stack<int, std::deque<int> > s;
+	s.push(42);
+	s.push(101);
+	s.push(69);
+
+	int i;
+
+	i = s.top();
+	if(69 != i){
+		printf("Popped %i instead of 69\n", i);
+		return false;
 	}
+	s.pop();
+	i = s.top();
+	if(101 != i){
+		printf("Popped %i instead of 101\n", i);
+		return false;
+	}
+	s.pop();
+	i = s.top();
+	if(42 != i){
+		printf("Popped %i instead of 42\n", i);
+		return false;
+	}
+	s.pop();
+
+	return s.size() == 0;
+}
+
+bool checkStackEquality(){
+	std::stack<int, std::deque<int> > s, t;
+	s.push(42);
+	s.push(101);
+	s.push(69);
+	t.push(42);
+	t.push(101);
+	t.push(69);
+	
+	return s == t;
+}
+
+
+int main(){
+
+	std::cout<<"Starting stack test" << std::endl;
+
+	TestFramework::init();
+
+	TestFramework::AssertReturns<unsigned long int>(checkStackSize, 3);
+	TestFramework::AssertReturns<bool>(checkStackPop, true);
+	TestFramework::AssertReturns<bool>(checkStackEquality, true);
+
+	TestFramework::results();
 
 	return 0;
 }
---
+++ tests/testframework.h
@@ -0,0 +1,37 @@
+#include <exception>
+#include <cstdio>
+#include <typeinfo>
+
+namespace TestFramework{
+	extern unsigned long int goodcount;
+	extern unsigned long int badcount;
+	extern unsigned long int totalcount;
+
+	unsigned long int getTotalTests();
+	unsigned long int getBadTests();
+	unsigned long int getGoodTests();
+
+	void init();
+	void results();
+
+	// The following functions take a pointer to a function return type T and compare
+	// it to the specified value.  If this is true then the goodcount is increased.
+	template <typename T> void AssertReturns(T (*f)(), const T val){
+		++totalcount;
+		printf(".");
+		T retval;
+		retval = f();
+		if( val == retval){
+			++goodcount;
+		}else{
+			printf("(%lu)", totalcount);
+			++badcount;
+		}
+	}
+
+
+
+}
+
+
+
