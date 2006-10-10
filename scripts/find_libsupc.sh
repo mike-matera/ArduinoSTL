314c4aa69d0e73212c2b0b4e44a23cb5312fda2c
Garrett Kajmowicz <gkajmowi@tbaytel.net>
2006-10-10 19:37:11 +0000

    Conversion of libsupc finding code from being in the make file to being in a script which can be modified by maintainers, etc.

---
+++ scripts/find_libsupc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This purpose of this file is to return the filename for libsupc++.a to be used.  We use the script so that it
+#can be easily replaced by system developers and cross-compilers so that the user doesn't require an installed
+#instance of libstdc++ on their system to use the library - just point at the appropriate location in the
+#gcc build directory and rip it apart.
+
+$CC -print-file-name=libsupc++.a
+
