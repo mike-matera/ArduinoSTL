6529f88c385d7d52a2c0989e2c8c73c0f0d975d9
埃博拉酱 <vhtmfu@outlook.com>
2022-12-23 20:56:49 +0800

    增加示例

---
+++ examples/CinAndCout/CinAndCout.ino
@@ -0,0 +1,18 @@
+#include <ArduinoSTL.h>
+
+using namespace std;
+
+void setup() {
+  Serial.begin(9600);
+  cout << "Feed me an integers." << endl;
+}
+
+void loop() {
+  int foo;
+  if (cin >> foo) { 
+    cout << "You fed me " << foo << endl;
+  }else{
+    cin.clear();
+    cin.ignore();
+  }
+}
