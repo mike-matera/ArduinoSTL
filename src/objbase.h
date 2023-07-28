c8e9092ad3ff67162641079a1c6f841640d749bf
Pavel Polunin <p.polunin@yandex.ru>
2023-07-28 09:43:24 +0300

    I studied, searched, that's what happened.

--- src/objbase.h
+++ src/objbase.h
@@ -4,48 +4,73 @@
 #include "type_traits"
 
 #ifdef __cplusplus
-template<typename Enum>
-inline constexpr Enum operator|(Enum a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum>(static_cast<T>(a) | static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+class auto_bool final
+{
+	Enum val_;
+public:
+	inline constexpr auto_bool(Enum val) : val_(val) {}
+	inline constexpr operator Enum() const { return val_; }
+	inline constexpr explicit operator bool() const
+	{
+		return static_cast<std::underlying_type_t<Enum>>(val_) != 0;
+	}
+};
+
+template <typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr auto_bool<Enum> operator&(Enum lhs, Enum rhs)
+{
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum>(
+		static_cast<T>(lhs) &
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum operator&(Enum a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum>(static_cast<T>(a) & static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum operator|(Enum lhs, Enum rhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum>(
+		static_cast<T>(lhs) |
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum operator^(Enum a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum>(static_cast<T>(a) ^ static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum operator^(Enum lhs, Enum rhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum>(
+		static_cast<T>(lhs) ^
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum& operator|=(Enum& a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum&>(static_cast<T&>(a) | static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum& operator|=(Enum& lhs, Enum rhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum&>(
+		static_cast<T&>(lhs) |
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum& operator&=(Enum& a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum&>(static_cast<T&>(a) & static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum& operator&=(Enum& lhs, Enum rhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum&>(
+		static_cast<T&>(lhs) &
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum& operator^=(Enum& a, Enum b) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum&>(static_cast<T&>(a) ^ static_cast<T>(b));
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum& operator^=(Enum& lhs, Enum rhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum&>(
+		static_cast<T&>(lhs) ^
+		static_cast<T>(rhs));
 }
 
-template<typename Enum>
-inline constexpr Enum operator~(Enum a) {
-	using T = std::underlying_type_t<Enum>;
-	return static_cast<Enum>(~static_cast<T>(a));
-
+template<typename Enum, typename = typename std::enable_if<std::is_enum<Enum>::value, Enum>::type>
+inline constexpr Enum operator~(Enum lhs) {
+	using T = typename std::underlying_type_t<Enum>;
+	return static_cast<Enum>(~static_cast<T>(lhs));
 }
-#endif
+#endif // __cplusplus
 
-#endif
+#endif // __OBJBASE_H
